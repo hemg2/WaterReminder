99ff5b16735f9f0a4064346c9b2e65eb47c76897
hemg2 <hemg2@naver.com>
2022-12-15 14:50:09 +0900

    Initial Commit

---
+++ ReminderWater/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReminderWater
+//
+//  Created by 1 on 2022/12/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
