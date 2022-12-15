d1d6d0a849afdd50507218f739abf2a4e681acf1
hemg2 <hemg2@naver.com>
2022-12-15 16:51:28 +0900

    style

--- View/AddAlertViewController.swift
+++ View/AddAlertViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class AddAlertViewController: UIViewController {
     
+    var pickedDate: ((_ date: Date) -> Void)?
+    
     @IBOutlet var datePicker: UIDatePicker!
     
     @IBAction func dismissButton(_ sender: UIBarButtonItem) {
+        
+        self.dismiss(animated: true)
     }
     
     @IBAction func saveButton(_ sender: UIBarButtonItem) {
+        pickedDate?(datePicker.date)
+        self.dismiss(animated: true)
     }
 }
